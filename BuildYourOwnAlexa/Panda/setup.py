2f4507467a185c09a23e6a597ec5d2de14d3e5e6
shanakrs <shanmuganathan.raju@cgi.com>
2021-09-29 14:19:11 +0530

    Initial Commit for building your own alexa using python

---
+++ BuildYourOwnAlexa/Panda/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Panda',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Lenovo',
+    author_email='shanakrs@outlook.com',
+    description='Panda AI'
+)
